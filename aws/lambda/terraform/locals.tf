95ac6ce6bfea59ff34de40f882ffccfdb9b92ac6
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-06-28 20:25:19 +0900

    update terraform

--- aws/lambda/terraform/locals.tf
+++ aws/lambda/terraform/locals.tf
@@ -3,5 +3,6 @@ locals {
   function_name  = "satoken-java-otel"
   handler        = "com.example.demo.lambda.DemoApplication::handleRequest"
   runtime        = "java17"
+  add_otel_layer = false
   otel_layer_arn = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel-java-agent-amd64-ver-1-26-0:2"
 }
