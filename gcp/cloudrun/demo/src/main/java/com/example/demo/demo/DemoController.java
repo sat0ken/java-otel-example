b65215cef4221ed291df29fac8fa6902c3162b8b
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-07-11 18:46:37 +0900

    cloud run用のソースを追加

---
+++ gcp/cloudrun/demo/src/main/java/com/example/demo/demo/DemoController.java
@@ -0,0 +1,44 @@
+package com.example.demo.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.cloud.opentelemetry.trace.TraceExporter;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class DemoController {
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @Bean
+    public SpanExporter googleTraceExporter() throws IOException {
+        return TraceExporter.createWithDefaultConfiguration();
+    }
+
+    @GetMapping("/rolldice")
+    public String index(@RequestParam("player") Optional<String> player) {
+        int result = this.getRandomNumber(1, 6);
+        if (player.isPresent()) {
+            logger.info("{} is rolling the dice: {}", player.get(), result);
+        } else {
+            logger.info("Anonymous player is rolling the dice: {}", result);
+        }
+        return "{\"number\" : %s}\n".formatted(Integer.toString(result));
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "{\"status\" : \"ok\"}\n";
+    }
+}
