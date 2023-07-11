b85008f70ce396319ae1392209cd7db1a5633230
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-07-11 11:52:12 +0900

    関数を追加

--- gcp/functions/demo/src/main/java/com/example/demo/DemoApplication.java
+++ gcp/functions/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import com.google.cloud.functions.HttpResponse;
 import com.google.cloud.opentelemetry.trace.TraceConfiguration;
 import com.google.cloud.opentelemetry.trace.TraceExporter;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
@@ -14,9 +16,12 @@ import io.opentelemetry.sdk.trace.export.SpanExporter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class DemoApplication implements HttpFunction {
     private static final String INSTRUMENTATION_SCOPE_NAME = DemoApplication.class.getName();
+    private static final Random random = new Random();
     private static OpenTelemetrySdk openTelemetrySdk;
     public DemoApplication() {
         TraceConfiguration configuration = TraceConfiguration.builder().setDeadline(Duration.ofMillis(30000)).build();
@@ -28,12 +33,29 @@ public class DemoApplication implements HttpFunction {
                     ).build()).buildAndRegisterGlobal();
     }
 
+    private static void doWork(String description) {
+        Span span = openTelemetrySdk.getTracer(INSTRUMENTATION_SCOPE_NAME).spanBuilder(description).startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            span.addEvent("doWork function");
+            Thread.sleep(100 + random.nextInt(5) * 100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            span.end();
+        }
+    }
+
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
         Span span = openTelemetrySdk.getTracer(INSTRUMENTATION_SCOPE_NAME).spanBuilder("service function").startSpan();
         BufferedWriter writer = response.getWriter();
         writer.write("Hello World!");
         span.addEvent("HTTP Request");
+        doWork("doWork called");
         span.end();
+        // Flush all bufferd traces
+        CompletableResultCode completableResultCode = openTelemetrySdk.getSdkTracerProvider().shutdown();
+        // wait till export finished
+        completableResultCode.join(10000, TimeUnit.MILLISECONDS);
     }
 }
