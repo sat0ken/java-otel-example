0de07c5080d85fb4f324efdfa5a6e14c54ba8f47
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-07-19 16:40:51 +0900

    手動計測のコードを追加

--- gcp/cloudrun/demo/src/main/java/com/example/demo/DemoController.java
+++ gcp/cloudrun/demo/src/main/java/com/example/demo/DemoController.java
@@ -1,21 +1,17 @@
 package com.example.demo;
 
-import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import io.opentelemetry.api.trace.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
 
-import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -23,33 +19,25 @@ import java.util.concurrent.ThreadLocalRandom;
 public class DemoController {
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
     private static final String INSTRUMENTATION_SCOPE_NAME = DemoController.class.getName();
+    private final Tracer tracer;
 
-    private static OpenTelemetrySdk openTelemetrySdk;
-
-//    public DemoController() {
-//        TraceConfiguration configuration = TraceConfiguration.builder().setDeadline(Duration.ofMillis(3000)).build();
-//        SpanExporter traceExporter = TraceExporter.createWithConfiguration(configuration);
-//        Resource resource = Resource.getDefault().merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "cloudrun-service")));
-//        openTelemetrySdk = OpenTelemetrySdk.builder()
-//                .setTracerProvider(
-//                        SdkTracerProvider.builder().addSpanProcessor(
-//                                BatchSpanProcessor.builder(traceExporter).build()
-//                        ).setResource(resource).build()).buildAndRegisterGlobal();
-//    }
+    @Autowired
+    DemoController(OpenTelemetry openTelemetry) {
+        tracer = openTelemetry.getTracer(DemoApplication.class.getName());
+    }
 
     @GetMapping("/rolldice")
     public String index(@RequestParam("player") Optional<String> player) {
-//        Span span = openTelemetrySdk.getTracer(INSTRUMENTATION_SCOPE_NAME).spanBuilder("/rolldice")
-//                .setSpanKind(SpanKind.SERVER).startSpan();
-//        span.addEvent("/rolldice called");
+        Span span = tracer.spanBuilder("/rolldice").startSpan();
+        span.addEvent("/rolldice called");
         int result = this.getRandomNumber(1, 6);
-        //span.setAttribute("result is", result);
+        span.setAttribute("result number", result);
         if (player.isPresent()) {
             logger.info("{} is rolling the dice: {}", player.get(), result);
         } else {
             logger.info("Anonymous player is rolling the dice: {}", result);
         }
-        //span.end();
+        span.end();
         return "{\"number\" : %s}\n".formatted(Integer.toString(result));
     }
 
